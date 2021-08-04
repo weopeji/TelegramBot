5cbb75bda742de971b82703dd4bb7ec388042f58
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-08-04 12:19:52 +0300

    1

--- html/project/document/index.js
+++ html/project/document/index.js
@@ -33,6 +33,16 @@
         templateText = templateText.replace(new RegExp("%bizznes_name%", 'g'), need_project.data.initials);
         templateText = templateText.replace(new RegExp("%invester_name%", 'g'), "_______________");
         templateText = templateText.replace(new RegExp("%inn%", 'g'), need_project.data.inn);
+        templateText = templateText.replace(new RegExp("%ogrn%", 'g'), need_project.data.ogrn);
+        templateText = templateText.replace(new RegExp("%addr%", 'g'), need_project.parce.addr);
+        templateText = templateText.replace(new RegExp("%city%", 'g'), need_project.parce.addr.split(',')[0]);
+        templateText = templateText.replace(new RegExp("%addr_f%", 'g'), need_project.data.addr);
+        templateText = templateText.replace(new RegExp("%bank%", 'g'), need_project.data.bank);
+        templateText = templateText.replace(new RegExp("%bik%", 'g'), need_project.data.bik);
+        templateText = templateText.replace(new RegExp("%state%", 'g'), need_project.data.account_get);
+        templateText = templateText.replace(new RegExp("%document%", 'g'), "_______________");
+        templateText = templateText.replace(new RegExp("%pay%", 'g'), "_______________");
+        templateText = templateText.replace(new RegExp("%pay_investor%", 'g'), "_______________");
 
         $('.index_page').append(templateText);
         $('.index_page p').eq(0).css("text-align", "center")
