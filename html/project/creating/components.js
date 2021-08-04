1acfc2e91b429130aba5b1c303dbd1a2c44c242f
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-08-04 16:07:55 +0300

    1

--- html/project/creating/components.js
+++ html/project/creating/components.js
@@ -756,7 +756,7 @@
                 _line.find('textarea').on('keyup input', function() 
                 {
                     var _val = $(this).val($(this).val().replace (/\D/, '').replace(/\s/g, ''));
-                    var format = _val.replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ');
+                    var format = String(_val).replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ');
                     $(this).val(format);
                 })
             }
