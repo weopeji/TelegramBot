52d2cbb2d67d6dbd54bdd02847c48660e450e897
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-09-04 01:07:47 +0300

    124

--- node/types/business.js
+++ node/types/business.js
@@ -49,8 +49,9 @@ async function getMoney(msg)
     var bar = new Promise((resolve, reject) => {
         _arrayProjects.forEach(async (value, index, array) => {
             var InvDocs = await InvDoc.find({projectId: value});
-            console.log(InvDocs);
-            allInv.push(InvDocs);
+            if(InvDocs.length > 0) {
+                allInv.push(InvDocs);
+            }
             if (index === array.length -1) resolve();
         })
     });
