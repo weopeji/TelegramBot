f33a200a774fb48a873c19341f229a9eb17816a5
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-09-29 08:57:49 +0300

    awdawd

--- html/assets/javascript/index/component.js
+++ html/assets/javascript/index/component.js
@@ -548,6 +548,65 @@
 
                     drawCircle('#4598d1', options.lineWidth, 100 / 100);
                     drawCircle('#0E1122', options.lineWidth, options.percent / 100);
+                },
+                thirds: function () {
+                    var template_text = $(`
+                        <div class="discret">
+                            <span class="discret_header">Выплаченная сумма2</span>
+                            <div class="wrappert">
+                                <div class="wrapper">
+                                    <div class="container">
+                                        <div id="validate-bar__parcent-bar3"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `);
+
+                    $('.index_page_body_data').append(template_text);
+
+                    var el = document.getElementById('validate-bar__parcent-bar3');
+
+                    var options = {
+                        percent: el.getAttribute('data-percent') || 0,
+                        size: el.getAttribute('data-size') || 220,
+                        lineWidth: el.getAttribute('data-line') || 10,
+                        rotate: el.getAttribute('data-rotate') || 0
+                    }
+
+                    var canvas = document.createElement('canvas');
+                    var span = document.createElement('span');
+
+                    span.textContent = options.percent;
+
+                    if (typeof(G_vmlCanvasManager) !== 'undefined') {
+                        G_vmlCanvasManager.initElement(canvas);
+                    }
+
+                    var ctx = canvas.getContext('2d');
+                    canvas.width = canvas.height = options.size;
+
+                    el.appendChild(span);
+                    el.appendChild(canvas);
+
+                    ctx.translate(options.size / 2, options.size / 2); // change center
+                    ctx.rotate((-1 / 2 + options.rotate / 180) * Math.PI); // rotate -90 deg
+
+                    //imd = ctx.getImageData(0, 0, 240, 240);
+                    var radius = (options.size - options.lineWidth) / 2;
+
+                    var drawCircle = function(color, lineWidth, percent) {
+                        percent = Math.min(Math.max(0, percent || 1), 1);
+                        ctx.beginPath();
+                        ctx.arc(0, 0, radius, 0, Math.PI * 2 * percent, false);
+                        ctx.strokeStyle = color;
+                        ctx.lineCap = 'square'; // butt, round or square
+                        ctx.lineWidth = lineWidth
+                        ctx.stroke();
+                    };
+
+                    drawCircle('#4598d1', options.lineWidth, 100 / 100);
+                    drawCircle('#0E1122', options.lineWidth, options.percent / 100);
                 }
             }
 
