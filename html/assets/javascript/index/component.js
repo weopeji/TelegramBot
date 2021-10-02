3e56f0f8acf76b0123b15d74914de7483a410c48
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-10-02 11:00:03 +0300

    123124

--- html/assets/javascript/index/component.js
+++ html/assets/javascript/index/component.js
@@ -75,6 +75,55 @@
                         <span>Перейти к диалогу</span>
                     </div>
                 `);
+
+                var headerPaysBlock = $(`
+                    <div class="headerPaysBlock">
+                        <div class="headerPaysBlock_header">
+                            <span class="headerPaysBlock_header_span">Выплаты по проекту</span>
+                            <div class="headerPaysBlock_header_line">
+                                <span>#</span>
+                                <span>Дата</span>
+                                <span>Сумма</span>
+                                <span>Статус</span>
+                            </div>
+                        </div>
+                        <div class="headerPaysBlock_body">
+                            <div class="headerPaysBlock_body_line">
+                                <span>1</span>
+                                <span>02.10.2022</span>
+                                <span>35 000 руб</span>
+                                <span class="headerPaysBlock_button">Прикреить чек</span>
+                            </div>
+                            <div class="headerPaysBlock_body_line">
+                                <span>2</span>
+                                <span>02.10.2023</span>
+                                <span>35 000 руб</span>
+                                <span class="headerPaysBlock_button">Прикреить чек</span>
+                            </div>
+                            <div class="headerPaysBlock_body_line">
+                                <span>3</span>
+                                <span>02.10.2024</span>
+                                <span>35 000 руб</span>
+                                <span class="headerPaysBlock_button">Прикреить чек</span>
+                            </div>
+                            <div class="headerPaysBlock_body_line">
+                                <span>4</span>
+                                <span>02.10.2025</span>
+                                <span>35 000 руб</span>
+                                <span class="headerPaysBlock_button">Прикреить чек</span>
+                            </div>
+                            <div class="headerPaysBlock_body_line">
+                                <span>5</span>
+                                <span>02.10.2026</span>
+                                <span>35 000 руб</span>
+                                <span class="headerPaysBlock_button">Прикреить чек</span>
+                            </div>
+                        </div>
+                    </div>
+                `);
+
+
+                $('.index_page_body_data').append(headerPaysBlock);
             } else {
 
                 var headerPaysBlock = $(`
