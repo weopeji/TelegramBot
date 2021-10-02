e1098dfa2f2aa8e01fbce9f62275830a14db0a11
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-10-02 08:42:19 +0300

    124124

--- html/assets/javascript/index/component.js
+++ html/assets/javascript/index/component.js
@@ -187,52 +187,52 @@
         }
     }
 
-    // class process_status 
-    // {
-    //     constructor() {};
+    class process_status 
+    {
+        constructor() {};
 
-    //     async render(allData) 
-    //     {
-    //         var _data = await callApi({
-    //             methodName: "invester_status_projects",
-    //             data: allData,
-    //         });
+        async render(allData) 
+        {
+            var _data = await callApi({
+                methodName: "invester_status_projects",
+                data: allData,
+            });
 
-    //         if(_data.length > 0) {
-    //             _data.forEach(function(element) 
-    //             {
-    //                 var statusBlock     = "";
-    //                 var statusInfo      = "";
+            if(_data.length > 0) {
+                _data.forEach(function(element) 
+                {
+                    var statusBlock     = "";
+                    var statusInfo      = "";
     
-    //                 if(!element.document) {
-    //                     statusBlock = "invester_status_project_yellow";
-    //                     statusInfo  = "Не подписан документ";
-    //                 } else {
-    //                     statusInfo  = "Деньги не переведенны";
-    //                 }
+                    if(!element.document) {
+                        statusBlock = "invester_status_project_yellow";
+                        statusInfo  = "Не подписан документ";
+                    } else {
+                        statusInfo  = "Деньги не переведенны";
+                    }
     
-    //                 var text = `
-    //                     <div class="invester_status_project ${statusBlock}">
-    //                         <p>Проект номер: ${element.projectId}</p>
-    //                         <p>${statusInfo}</p>
-    //                     </div>
-    //                 `;
+                    var text = `
+                        <div class="invester_status_project ${statusBlock}">
+                            <p>Проект номер: ${element.projectId}</p>
+                            <p>${statusInfo}</p>
+                        </div>
+                    `;
     
-    //                 $('.content').append(text);
-    //             })
-    //         } else {
-    //             var statusBlock = "invester_status_project_red";
+                    $('.content').append(text);
+                })
+            } else {
+                var statusBlock = "invester_status_project_red";
 
-    //             var text = `
-    //                 <div class="invester_status_project ${statusBlock}">
-    //                     <p>Вы не подавали заявку на инвестирование</p>
-    //                 </div>
-    //             `;
+                var text = `
+                    <div class="invester_status_project ${statusBlock}">
+                        <p>Вы не подавали заявку на инвестирование</p>
+                    </div>
+                `;
 
-    //             $('.content').append(text);
-    //         }
-    //     }
-    // }
+                $('.content').append(text);
+            }
+        }
+    }
 
     class pay_investors {
         constructor() {};
@@ -1064,6 +1064,7 @@
             myProjects,
             signature,
             reward,
+            process_status,
         }
     }
 
