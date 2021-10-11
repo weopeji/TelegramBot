5121d3baad53b57126f501d0e0a3fc200bbbba67
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-10-11 07:24:26 +0300

    awdawd

--- html/project/document/index.js
+++ html/project/document/index.js
@@ -29,7 +29,7 @@
 
         var templateText = need_project.signature_document.document_html.toString();
 
-        templateText = templateText.replace(new RegExp("%date_now%", 'g'), "04.08.2021");
+        templateText = templateText.replace(new RegExp("%date_now%", 'g'), new Date());
         templateText = templateText.replace(new RegExp("%bizznes_name%", 'g'), need_project.data.initials);
         templateText = templateText.replace(new RegExp("%invester_name%", 'g'), "_______________");
         templateText = templateText.replace(new RegExp("%inn%", 'g'), need_project.data.inn);
@@ -40,8 +40,10 @@
         templateText = templateText.replace(new RegExp("%bank%", 'g'), need_project.data.bank);
         templateText = templateText.replace(new RegExp("%bik%", 'g'), need_project.data.bik);
         templateText = templateText.replace(new RegExp("%state%", 'g'), need_project.data.account_get);
+        templateText = templateText.replace(new RegExp("%date_pay%", 'g'), need_project.data.collection_period);
         templateText = templateText.replace(new RegExp("%pay%", 'g'), "_______________");
         templateText = templateText.replace(new RegExp("%pay_investor%", 'g'), "_______________");
+        templateText = templateText.replace(new RegExp("%document_more%", 'g'), "_______________");
 
         if(typeof need_project.signature_document.img != "undefined")
         {
