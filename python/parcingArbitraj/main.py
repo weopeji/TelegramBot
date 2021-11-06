09770c29c38c1542bb36f534c86ed5052aaf4235
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-11-06 12:20:41 +0300

    awdwad

--- python/parcingArbitraj/main.py
+++ python/parcingArbitraj/main.py
@@ -1,16 +1,20 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support import expected_conditions as EC
 from selenium_stealth import stealth
 import json
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
-
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
-driver = webdriver.Chrome(options=options, executable_path="/usr/lib/chromium-browser/chromedriver")
+
+s = Service(ChromeDriverManager().install())
+
+driver = webdriver.Chrome(options=options, service=s)
 
 stealth(driver,
         languages=["en-US", "en"],
