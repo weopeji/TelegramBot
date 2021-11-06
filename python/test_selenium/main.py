446ae58a609fdefd7b53992d659f99a616f83760
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-11-06 13:40:27 +0300

    awdawd

--- python/test_selenium/main.py
+++ python/test_selenium/main.py
@@ -2,12 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 s = Service(ChromeDriverManager().install())
 
-driver = webdriver.Chrome(service=s)
-driver.maximize_window()
+driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
 
 driver.get("http://www.python.org")
 
