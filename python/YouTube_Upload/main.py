695a34e9285ae8b603e83da2b774381209e91f70
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-11-12 08:06:06 +0300

    awd

--- python/YouTube_Upload/main.py
+++ python/YouTube_Upload/main.py
@@ -3,7 +3,7 @@ from Google import Create_Service
 from googleapiclient.http import MediaFileUpload
 import sys
 
-CLIENT_SECRET_FILE = 'client_secret.json'
+CLIENT_SECRET_FILE = '/var/www/python/YouTube_Upload/client_secret.json'
 API_NAME = 'youtube'
 API_VERSION = 'v3'
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
