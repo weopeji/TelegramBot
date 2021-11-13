555df76008bc172af89f048e1f87dd4b3059ae28
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-11-13 15:32:00 +0300

    Update component.js

--- html/assets/javascript/index/component.js
+++ html/assets/javascript/index/component.js
@@ -117,6 +117,7 @@
             function toDateTime(secs) {
                 var t = new Date(1970, 0, 1); // Epoch
                 t.setSeconds(secs);
+                t.toLocaleDateString();
                 return t;
             }
 
