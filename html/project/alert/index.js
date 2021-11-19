6e9b61df8ab7f2e8192d6b1640073924366c0a90
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-11-19 11:13:18 +0300

    awd

--- html/project/alert/index.js
+++ html/project/alert/index.js
@@ -1,6 +1,7 @@
 ((global) => {
     var _data = new Date();
     $('.alert span').html(`${_data.getDate()}.${_data.getMonth() + 1}.${_data.getFullYear()}`);
+    $('.alert p').html(_GET("text"));
     $('body').append('<div class="all_good"></div');
 })(window)
 
