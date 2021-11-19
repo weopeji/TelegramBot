085fdaa8707a8057a524e983070d6a03c5959609
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-11-19 12:09:13 +0300

    Update index.js

--- html/project/alert/index.js
+++ html/project/alert/index.js
@@ -1,7 +1,12 @@
 ((global) => {
+    function fixedEncodeURI (str) {
+        return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
+    }
+
+    var _textEncode = fixedEncodeURI(_GET("text"));
     var _data = new Date();
     $('.alert span').html(`${_data.getDate()}.${_data.getMonth() + 1}.${_data.getFullYear()}`);
-    $('.alert p').html(_GET("text"));
+    $('.alert p').html(_textEncode);
     $('body').append('<div class="all_good"></div');
 })(window)
 
