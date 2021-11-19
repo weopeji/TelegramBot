25e1852ca2fe42cf8a01f8a7c1f0c9f323ad7f81
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-11-19 11:08:45 +0300

    Update index.js

--- html/project/alert/index.js
+++ html/project/alert/index.js
@@ -1,5 +1,5 @@
 ((global) => {
-    var _data = new Data();
+    var _data = new Date();
     $('.alert span').html(`${_data.getDate()}.${_data.getMonth()}.${_data.getFullYear()}`);
     $('body').append('<div class="all_good"></div');
 })(window)
