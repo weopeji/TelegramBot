48c3505a11f11dcdcd8848a520047dc39a34c10a
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-11-24 15:32:25 +0300

    Update components.js

--- settings/js/components.js
+++ settings/js/components.js
@@ -1397,8 +1397,15 @@
                     <h1>Загрузка видео на YouTube</h1>
                     <video controls="controls" src="https://invester-relocation.site/projects/${_project._id}/${_project.data['file+8']}"></video>
                     <div class="upload_video_block_unputs">
-                        <textarea name="name">${_project.data.name}</textarea>
-                        <textarea name="description">${_project.data.target}</textarea>
+                        <textarea name="name">${_project.data.name} № ${_project._id}</textarea>
+                        <textarea name="description">
+                            ${_project.data.name} № ${_project._id}<br>
+                            ${_project.data.target}<br>
+                            Ставка ${_project.data.rate * 12}% годовых<br>
+                            Выплаты ${_project.data.date_payments}<br>
+                            Вход от ${_project.data.minimal_amount} руб.
+                            Подробнее о предложении в телеграм канале - https://t.me/invester_official
+                        </textarea>
                     </div>
                 </div>
             `);
