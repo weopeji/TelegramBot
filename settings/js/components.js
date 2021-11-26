bff0e70087d756b79e90d81c00175a48f39b6afa
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-11-26 13:14:09 +0300

    awd

--- settings/js/components.js
+++ settings/js/components.js
@@ -1313,8 +1313,8 @@
             `);
 
             tamplateText.find('.upload_video_block_button').click( function () {
-                var _name = $('#upload_video_name').text();
-                var _description = $('#upload_video_description').text();
+                var _name = $('#upload_video_name').html();
+                var _description = $('#upload_video_description').html();
                 alert(`
                     name: ${_name} \n
                     description: ${_name}
