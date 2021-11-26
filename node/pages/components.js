2c67bf9e4ab80e9735ed4e69f4cf80f56fca73b0
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-11-26 15:04:51 +0300

    Update components.js

--- node/pages/components.js
+++ node/pages/components.js
@@ -995,8 +995,8 @@ async function acceptProject(socket,data,callback)
         clip: {
             x: bounding_box.x,
             y: bounding_box.y,
-            width: Math.min(bounding_box.width, page.viewport().width),
-            height: Math.min(bounding_box.height, page.viewport().height),
+            width: 1024,
+            height: 1024,
         },
     });
     await browser.close();
