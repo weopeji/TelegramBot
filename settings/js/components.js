c353d336d014683d3a79526a5704da67bbea9839
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-12-11 16:47:34 +0300

    awd

--- settings/js/components.js
+++ settings/js/components.js
@@ -1512,7 +1512,7 @@
                         <span>Редактирование Кратности</span>
                     </div>
                     <div class="body_point_line_block_more_frame">
-                        <iframe src="fssp.gov.ru/iss/ip" frameborder="0"></iframe>
+                        <iframe src="https://fssp.gov.ru/iss/ip"></iframe>
                     </div>
                 </div>
             `);
