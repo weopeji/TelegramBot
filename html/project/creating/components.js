975dcd59fd6fc9f59e1965ec30e72c30de50a469
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2021-12-31 03:04:28 +0300

    awd

--- html/project/creating/components.js
+++ html/project/creating/components.js
@@ -1091,16 +1091,16 @@
 
                     console.log(_data);
 
-                    $input.find('.body_point_line_more_info_addr span').eq(0).text(_data[0].result);
-                    $input.find('.body_point_line_more_info_addr span').eq(1).text(_data[0].source);
-                    $input.find('.body_point_line_more_info_addr').fadeOut( function () {
+                    $input.parent().find('.body_point_line_more_info_addr span').eq(0).text(_data[0].result);
+                    $input.parent().find('.body_point_line_more_info_addr span').eq(1).text(_data[0].source);
+                    $input.parent().find('.body_point_line_more_info_addr').fadeOut( function () {
                         $(this).fadeIn();
                     });
 
-                    $input.find('.body_point_line_more_info_addr span').click( function () {
+                    $input.parent().find('.body_point_line_more_info_addr span').click( function () {
                         var inputText = $(this).text();
                         $(this).parent().parent().find('input').val(inputText);
-                        $input.find('.body_point_line_more_info_addr').fadeOut();
+                        $input.parent().find('.body_point_line_more_info_addr').fadeOut();
                     })
                 }
 
