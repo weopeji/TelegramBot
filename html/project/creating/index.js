5a9fcaf0e2f696c6fb169473d9992526fd226558
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-01-04 12:01:11 +0300

    фцв

--- html/project/creating/index.js
+++ html/project/creating/index.js
@@ -55,7 +55,7 @@
                     global._typePage = 'signature_document';
 
                     $('.index_page_body h1').html('Подписание документов');
-                    $('.index_page_body h2').html('Подпишите составленный документ и загрузите его');
+                    $('.index_page_body h2').html('Скачайте и подпишите составленный документ, затем загрузите его обратно');
                     $('.index_page_body_button span').html('Отправить');
 
                     await _components.render_signature_document(_project);
