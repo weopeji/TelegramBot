20bef8fbd708882dc43a87071c55a6c1d330a3ad
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-01-12 11:49:40 +0300

    Update parcing.js

--- node/pages/parcing.js
+++ node/pages/parcing.js
@@ -6,110 +6,113 @@ module.exports = {
     ParceUsersBlock,
 }
 
+async function deistvitelenLiPaspport(_query)
+{
+    return new Promise((resolve,reject) => 
+    {
+        var url     = "https://cleaner.dadata.ru/api/v1/clean/passport";
+        var token   = "cd3a829357362fec55fc201c3f761002def9906f";
+        var secret  = "17df8cdd3e4ace2be6c66bd1ee208d26e54d9843";
+        var query   = _query;
+
+        var options = {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Token " + token,
+                "X-Secret": secret
+            },
+            body: JSON.stringify([query])
+        }
+
+        fetch(url, options)
+        .then(response => response.text())
+        .then(result => resolve(result))
+        .catch(error => console.log("error", error));
+    })
+}
+
+async function yavlaetcaLiSamozanyatim(_inn)
+{
+    return new Promise((resolve,reject) =>
+    {
+        const dateStr = new Date().toISOString().substring(0, 10);
+        const url = "https://statusnpd.nalog.ru/api/v1/tracker/taxpayer_status";
+        const data = {
+            inn: _inn,
+            requestDate: dateStr,
+        };
+        resp = fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        resolve(resp);
+    });
+}
+
+async function arbitrajnayaPraktikaFizLica(_initials, _region, date_user)
+{
+    console.log(_initials);
+    console.log(_region);
+    console.log(date_user);
+
+    return new Promise((resolve,reject) =>
+    {
+        var fio             = _initials.split(' ');
+        var query           = _region;
+        var url             = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
+        var token           = "cd3a829357362fec55fc201c3f761002def9906f";
+        var first_name      = fio[1];
+        var second_name     = fio[2];
+        var last_name       = fio[0];
+        var birth_date      = date_user;
+
+        var options = {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": "Token " + token
+            },
+            body: JSON.stringify({query: query})
+        }
+
+        fetch(url, options)
+        .then(response => response.text())
+        .then(result => 
+        {
+            var _dataFirst = JSON.parse(result.toString()).suggestions[0].data.region_kladr_id;
+
+            _dataFirst = _dataFirst.replace(/0/g, '');
+
+            var config = {
+                method: 'get',
+                url: `https://api-ip.fssp.gov.ru/api/v1.0/search/physical?token=er77gLcQvTO5&firstname=${encodeURI(first_name)}&secondname=${encodeURI(second_name)}&lastname=${encodeURI(last_name)}&birthdate=${birth_date}&region=${_dataFirst}`,
+                headers: { }
+            };    
+            
+            axios(config)
+            .then(async function (response) {
+                var adaw = await R_F.create({
+                    data: JSON.stringify(response.data),
+                })
+                resolve(adaw._id);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        });
+    });
+};
 
 async function ParceUsersBlock(_project, MoreUsers)
 {
-    async function deistvitelenLiPaspport(_query)
-    {
-        return new Promise((resolve,reject) => 
-        {
-            var url     = "https://cleaner.dadata.ru/api/v1/clean/passport";
-            var token   = "cd3a829357362fec55fc201c3f761002def9906f";
-            var secret  = "17df8cdd3e4ace2be6c66bd1ee208d26e54d9843";
-            var query   = _query;
-    
-            var options = {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Token " + token,
-                    "X-Secret": secret
-                },
-                body: JSON.stringify([query])
-            }
-    
-            fetch(url, options)
-            .then(response => response.text())
-            .then(result => resolve(result))
-            .catch(error => console.log("error", error));
-        })
-    }
-
-    async function yavlaetcaLiSamozanyatim(_inn)
-    {
-        return new Promise((resolve,reject) =>
-        {
-            const dateStr = new Date().toISOString().substring(0, 10);
-            const url = "https://statusnpd.nalog.ru/api/v1/tracker/taxpayer_status";
-            const data = {
-                inn: _inn,
-                requestDate: dateStr,
-            };
-            resp = fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            });
-
-            resolve(resp);
-        });
-    }
-
-    async function arbitrajnayaPraktikaFizLica(_initials, _region, date_user)
-    {
-        return new Promise((resolve,reject) =>
-        {
-            var fio             = _initials.split(' ');
-            var query           = _region;
-            var url             = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
-            var token           = "cd3a829357362fec55fc201c3f761002def9906f";
-            var first_name      = fio[1];
-            var second_name     = fio[2];
-            var last_name       = fio[0];
-            var birth_date      = date_user;
-
-            var options = {
-                method: "POST",
-                mode: "cors",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json",
-                    "Authorization": "Token " + token
-                },
-                body: JSON.stringify({query: query})
-            }
-
-            fetch(url, options)
-            .then(response => response.text())
-            .then(result => 
-            {
-                var _dataFirst = JSON.parse(result.toString()).suggestions[0].data.region_kladr_id;
-
-                _dataFirst = _dataFirst.replace(/0/g, '');
-
-                var config = {
-                    method: 'get',
-                    url: `https://api-ip.fssp.gov.ru/api/v1.0/search/physical?token=er77gLcQvTO5&firstname=${encodeURI(first_name)}&secondname=${encodeURI(second_name)}&lastname=${encodeURI(last_name)}&birthdate=${birth_date}&region=${_dataFirst}`,
-                    headers: { }
-                };    
-                
-                axios(config)
-                .then(async function (response) {
-                    var adaw = await R_F.create({
-                        data: JSON.stringify(response.data),
-                    })
-                    resolve(adaw._id);
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-            });
-        });
-    };
-
     return new Promise(async (resolve,reject) => 
     {
         var _data = 
