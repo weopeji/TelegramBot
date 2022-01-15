5cdb1ae042b249ed982c01232a2444eab169b3d5
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-01-15 12:09:52 +0300

    awd

--- settings/js/redacting_page.js
+++ settings/js/redacting_page.js
@@ -105,7 +105,7 @@
                 if(typeof _project.data[DataBlock._id] != "undefined")
                 {
                     _block.find('span').click( function() {
-                        var _url = getURL() + "/projects/" + _project._id + "/" + $(this).parent().parent().attr('data');
+                        var _url = getURL() + "/projects/" + _project._id + "/" + $(this).parent().parent().find('BB');
                         window.open(_url);
                     })
                 }
