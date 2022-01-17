5fcc624f460d90a423b06f82d97b0f59075f3e5f
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-01-17 11:21:57 +0300

    awd

--- html/project/creating/components.js
+++ html/project/creating/components.js
@@ -1101,11 +1101,102 @@
 
         render_signature(_project) 
         {
+
+            var _dataNEedPutNew = {
+                "1": {
+                    header: "Загрузите документы",
+                    body: [
+                        {
+                            type: "file",
+                            name: `Устав`,
+                            _id: "file+1_s"
+                        },
+                        {
+                            type: "file",
+                            name: `Решение об избрании руководителя`,
+                            _id: "file+2_s"
+                        },
+                        {
+                            type: "file",
+                            name: `Приказ о назначении руководителя`,
+                            _id: "file+3_s"
+                        },
+                        {
+                            type: "file",
+                            name: `Свидетельство ИНН`,
+                            _id: "file+4_s"
+                        },
+                        {
+                            type: "file",
+                            name: `ОГРН`,
+                            _id: "file+5_s"
+                        },
+                        {
+                            type: "file",
+                            name: `Карточка с банковскими реквизитами`,
+                            _id: "file+6_s"
+                        },
+                        {
+                            type: "file",
+                            name: `Бух.отчетность за последний год`,
+                            _id: "file+7_s"
+                        },
+                    ]
+                },
+                "2": {
+                    header: "Загрузите документы",
+                    body: [
+                        {
+                            type: "file",
+                            name: `Приказ о назначении руководителя`,
+                            _id: "file+3_s"
+                        },
+                        {
+                            type: "file",
+                            name: `Свидетельство ИНН`,
+                            _id: "file+4_s"
+                        },
+                        {
+                            type: "file",
+                            name: `ОГРН`,
+                            _id: "file+5_s"
+                        },
+                        {
+                            type: "file",
+                            name: `Карточка с банковскими реквизитами`,
+                            _id: "file+6_s"
+                        },
+                        {
+                            type: "file",
+                            name: `Бух.отчетность за последний год`,
+                            _id: "file+7_s"
+                        },
+                    ]
+                },
+                "3": {
+                    header: "Загрузите документы",
+                    body: [
+                        {
+                            type: "file",
+                            name: `Карточка с банковскими реквизитами`,
+                            _id: "file+6_s"
+                        },
+                        {
+                            type: "file",
+                            name: `Бух.отчетность за последний год`,
+                            _id: "file+7_s"
+                        },
+                    ]
+                },
+            }
+
+
+
             var _type = _project.signature.type;
 
-            for (var key in this.signature[_type]) 
-            {
-                var data = this.signature[_type][key];
+            // for (var key in _dataNEedPutNew[_project.data.organization]) 
+            // {
+                var data = _dataNEedPutNew[_project.data.organization];
 
                 var _body = $(`<div class="body_point"></div>`);
                 _body.append(`
@@ -1121,7 +1212,7 @@
                 });
 
                 $('.index_page_body_points').append(_body);
-            }
+            // }
             
         }
 
