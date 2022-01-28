6c4106fa621b6ae997af545494696e821d0f990f
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-01-28 16:30:06 +0300

    фцв

--- html/project/creating/components.js
+++ html/project/creating/components.js
@@ -970,7 +970,7 @@
             })
 
             newBlock.find('.creating_page_input_div[data="open"]').click( function() {
-                window.open(`/projects/${_project._id}/file_registration_document.${global.fileRegMorePts}` , '_blank');
+                window.open(`/projects/${_project._id}/file_signature_document.${global.fileRegMorePts}` , '_blank');
             })
 
             newBlock.find('.creating_page_input_div[data="relocation"]').click( function() {
