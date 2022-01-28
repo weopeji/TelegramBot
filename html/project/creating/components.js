4e81740ffdaa473a42e3e4d44acffda9f20e8ba4
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-01-28 12:49:35 +0300

    awd

--- html/project/creating/components.js
+++ html/project/creating/components.js
@@ -936,6 +936,22 @@
                 </div>
             `);
 
+            var newBlock = $(`
+                <div class="creating_page_input creating_page_input_document_img">
+                    <img src="./html/assets/images/documentPNG.png" alt="">
+                    <div class="creating_page_input_document_img_hover">
+                        <div class="creating_page_input_document_img_hover_row">
+                            <span>Скачать договор</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="creating_page_input">
+                    <div class="creating_page_input_div" data="pay">
+                        <span style="text-align: center">Загрузить подписаный</span> 
+                    </div>
+                </div>
+            `);
+
             _block.find('.body_point_adder_document_block_buttons_button[data="download"]').click( function() {
                 window.open(`https://invester-relocation.site/projects/${_project._id}/signature_document.pdf`, "_blank");
             })
@@ -944,7 +960,7 @@
                 $(this).parent().find('input').trigger('click');
             })
 
-            $('.index_page_body_points').append(_block);
+            $('.index_page_body_points').append(newBlock);
         }
 
         render_registration_document(_project) 
