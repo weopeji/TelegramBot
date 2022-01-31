6bb1a178e47cda3dbf893bc0be161d00ff436140
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-01-31 13:00:48 +0300

    awd

--- html/assets/javascript/main.js
+++ html/assets/javascript/main.js
@@ -284,7 +284,7 @@ function DateFormatted(data)
         return new Array(width - s.toString().length + 1).join(character) + s;
     }
 
-    var maxDate             = new Date(data);
+    var maxDate             = new Date(Number(data.toString()));
     return pad(maxDate.getDate(), 2, '0') + '.' + pad(maxDate.getMonth() + 1, 2, '0') + '.' + maxDate.getFullYear();
 }
 
