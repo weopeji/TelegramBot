e7ba9b706736ea038b1d379305ca083ed2daa1e1
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-02 14:51:38 +0300

    awd

--- settings/js/commissions.js
+++ settings/js/commissions.js
@@ -67,7 +67,7 @@
                         <span>${commissionsElement.invDoc.data.pay.toString().ReplaceNumber()}</span>
                         <span>${commissionsElement.commissionInvestER}</span>
                         <span>Подтвердить</span>
-                        <a href="google.com">Написать</a>
+                        <span><a href="google.com">Написать</a></span>
                     </div>
                 `);
 
@@ -119,7 +119,7 @@
                         <span>${commissionsElement.invDoc.data.pay.toString().ReplaceNumber()}</span>
                         <span>${commissionsElement.commissionInvestER}</span>
                         <span>Подтвердить</span>
-                        <a href="google.com">Написать</a>
+                        <span><a href="google.com">Написать</a></span>
                     </div> 
                 `);
 
