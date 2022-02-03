884bd195357c248687926780ff26a70a7cb7b9b5
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-04 01:36:47 +0300

    фцв

--- html/assets/javascript/index/telegram_authorization.js
+++ html/assets/javascript/index/telegram_authorization.js
@@ -18,9 +18,24 @@
     {
         constructor() {};
 
+        async renderStyles()
+        {
+            $('.index_page_menu').css({
+                "flex": "none",
+                "position": "absolute",
+                "margin-left": "-100%",
+            });
+
+            $('.index_page_body_header_info').css({
+                "justify-content": "center",
+            });
+
+            $('.index_page_body_header_info span').html("Telegram Авторизация");
+        }
+
         async render()
         {
-            
+
         }
     }
 
