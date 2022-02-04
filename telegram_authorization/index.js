c9a769885122162da7bdb411627a629d1202fad7
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-04 13:53:48 +0300

    awd

--- telegram_authorization/index.js
+++ telegram_authorization/index.js
@@ -1,5 +1,5 @@
 window.Telegram.Login.auth(
-    { bot_id: '2062839693:AAE0hzj8SVXyexq29s5x7aRLC5x8O77c-pQ', request_access: true },
+    { bot_id: 'invester_official_bot', request_access: true },
     (data) => {
         console.log('data');
     }
