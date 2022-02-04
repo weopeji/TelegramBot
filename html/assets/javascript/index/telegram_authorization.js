1be2a80e38d0a36eba01a3d0151e19ae7a45e3b5
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-04 13:41:00 +0300

    awd

--- html/assets/javascript/index/telegram_authorization.js
+++ html/assets/javascript/index/telegram_authorization.js
@@ -104,7 +104,9 @@
             global.onTelegramAuth = (user) => {
                 console.log(user);
                 alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-            }
+            };
+
+            TWidgetLogin.auth();
         }
 
         async render()
