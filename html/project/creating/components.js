f25774aa7db1e03a4c3f79e3a7d4007510274109
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-07 19:56:09 +0300

    awd

--- html/project/creating/components.js
+++ html/project/creating/components.js
@@ -775,7 +775,7 @@
                     var preloaderBlockBlock = $(`
                         <div class="preloaderBlockBlock">
                             <div class="preloaderBlockBlock_row">
-                                <p>Вводя персональные данные физических лиц, вы подтверждаете, что ознакомились с <a href="/documents/p.pdf" target="_blank">Политикой обработки персональных данных</a> и даете согласие на обработку персональных данных</p>
+                                <p>Вводя персональные данные физических лиц, вы подтверждаете, что ознакомились с <a href="/documents/p5.pdf" target="_blank">Политикой обработки персональных данных</a> и даете согласие на обработку персональных данных</p>
                                 <div class="preloaderBlockBlock_button">
                                     <span>Подтверждаю</span>
                                 </div>
