660b9d5b6dbb97f3a0d7e172aa58c07cf9037387
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-12 21:58:48 +0300

    awd

--- settings/js/redacting_page.js
+++ settings/js/redacting_page.js
@@ -366,11 +366,6 @@
                         </div>
                     `);
 
-                    if(_needElementSort.type == "file")
-                    {
-                        _block.find('input[type="checkbox"]').remove();
-                    }
-
                     _block.find(`.structCreator_new_block_buttons_block[data="input"]`).click( function()
                     {
                         if($(this).parent().parent().parent().parent().attr('type') == "file")
