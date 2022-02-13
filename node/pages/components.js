04ac6925243c59b7d9dc21e77b400bbd6a3ef104
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-13 21:48:09 +0300

    awd

--- node/pages/components.js
+++ node/pages/components.js
@@ -1457,7 +1457,7 @@ async function acceptInvestor(socket,data,callback)
     var InvPay              = Number(_InvDoc.data.pay.toString().replace(/\s/g, ''));       // 100 000
     var ProjectDate         = Number(_Project.data.date.toString().replace(/\s/g, ''));     // 2 мес \ Бессрочно
     var jsDate              = new Date(InvPay.date);
-    var NowToday            = DateTime.fromJSDate(jsDate).setZone("Europe/Moscow");
+    var NowToday            = DateTime.fromJSDate(jsDate);
     console.log(NowToday);
     var InvPays             = [];
     
