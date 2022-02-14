71afd450e99a751157f616df68272b2f3ef5b8cb
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-14 14:56:52 +0300

    awd

--- html/assets/javascript/index/chat.js
+++ html/assets/javascript/index/chat.js
@@ -35,7 +35,7 @@
 
                 if(element.img)
                 {
-                    _PathUrl = element.img.file_path
+                    _PathUrl = `https://api.telegram.org/file/bot2062839693:AAE0hzj8SVXyexq29s5x7aRLC5x8O77c-pQ/` + element.img.file_path;
                 }
 
                 var template_text = $(`
