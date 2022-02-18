b91f4290e3ee0e3b7263100c9fb24ef1c4d5b421
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-18 13:28:30 +0300

    awd

--- html/project/youtube_cover/index.js
+++ html/project/youtube_cover/index.js
@@ -1,5 +1,5 @@
 (async (global) => {
-    
+
     const callApi = ({ methodName, data }) => {    
         return new Promise((resolve, reject) => 
         {
@@ -13,20 +13,28 @@
         });
     }
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-    }
-
-    $('img').attr('src', `./images/${getRandomInt(8)}.jpg`);
-
-    var _id = _GET('id');
-
-    var need_project = await callApi({
-        methodName: 'getProject',
-        data: _id,
+    io_connect( function() 
+    {
+        Main();
     });
 
-    $('.index_page_block_body h1').html(need_project.data.name);
-    $('.index_page_block_target span').html(need_project.data.target);
+    async function Main()
+    {
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max);
+        }
+    
+        $('img').attr('src', `./images/${getRandomInt(8)}.jpg`);
+    
+        var _id = _GET('id');
+    
+        var need_project = await callApi({
+            methodName: 'getProject',
+            data: _id,
+        });
+    
+        $('.index_page_block_body h1').html(need_project.data.name);
+        $('.index_page_block_target span').html(need_project.data.target);
+    }
 
 })(window)
