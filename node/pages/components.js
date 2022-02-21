2aaef5513558915275fb19bf8d5c17b73e81b4bb
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-21 03:14:46 +0300

    awd

--- node/pages/components.js
+++ node/pages/components.js
@@ -2218,7 +2218,7 @@ async function getNewDataProjects(socket,data,callback)
 {
     var _project = await Project.findOne({_id: data._id});
 
-    h.full_alert_user(_project.user, `К проекту номер ${_project._id} запрошенны дополнительные документы`, `correction_signature`);
+    h.full_alert_user(_project.user, `К проекту номер ${_project._id} "${_project.data.name}" запрошенны дополнительные документы`, `correction_signature`);
 
     var _project = await Project.findOneAndUpdate({_id: data._id}, 
     {
