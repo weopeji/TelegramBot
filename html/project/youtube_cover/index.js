530204f2b3c261956e0ee2c78d04824ce9695638
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-25 14:42:23 +0300

    awd

--- html/project/youtube_cover/index.js
+++ html/project/youtube_cover/index.js
@@ -32,12 +32,19 @@
             methodName: 'getProject',
             data: _id,
         });
+
+        var _data = need_project.data.collection_period.split("-");
     
         $('.index_page_block_body h1').html(need_project.data.name);
         $('.index_page_block_target span').html(need_project.data.target);
 
         $('.index_page_blockMoneys_line_second').find('span').eq(0).html(need_project.data.rate + "%");
         $('.index_page_blockMoneys_line_second').find('span').eq(1).html(need_project.data.minimal_amount + " ₽");
+
+       $('.index_page_block_data_line_second').find('span').eq(0).html(need_project.data.attraction_amount + " ₽");
+       $('.index_page_block_data_line_second').find('span').eq(1).html(need_project.data.date_payments);
+       $('.index_page_block_data_line_second').find('span').eq(2).html(need_project.data.date + " мес");
+       $('.index_page_block_data_line_second').find('span').eq(3).html(`${_data[2]}.${_data[1]}.${_data[0]}`);
     }
 
 })(window)
