4243f596d61790fdea4b0748ff1eff2b64f2bbc9
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-02-26 01:30:53 +0300

    awd

--- html/assets/javascript/index/chat.js
+++ html/assets/javascript/index/chat.js
@@ -16,46 +16,98 @@
 
     class chat
     {
-        async render()
+        async renderChat()
         {
-            var getChats     = await callApi({
-                methodName: "getChats",
-                data:  global.allData._id,
+            var getChat = await callApi({
+                methodName: "getChatsOfId",
+                data: {
+                    user: global.allData._id,
+                    id: _GET("id")
+                },
             });
 
-            var block = $(`
-                <div class="msg_block_getting">
+            var templateText = $(`
+                    <div class="chat_block">
+                        <div class="chat_block_chat">
+                            <div class="chat_block_chat_header">
+                                <span>Чат</span>
+                            </div>
+                            <div class="chat_block_chat_body">
+                                <div class="chat_block_chat_body_row">
 
-                </div>
-            `);
+                                    <div class="chat_block_chat_body_msgs">
 
-            getChats.forEach(element => {
+                                    </div>
 
-                var _PathUrl = null;
-
-                if(element.img)
-                {
-                    _PathUrl = `https://api.telegram.org/file/bot2062839693:AAE0hzj8SVXyexq29s5x7aRLC5x8O77c-pQ/` + element.img.file_path;
-                }
-
-                var template_text = $(`
-                    <div class="msg_block_getting_line">
-                        <div class="msg_block_getting_line_img">
-                            <div class="msg_block_getting_line_img_block">
-                                <img src="${_PathUrl}" alt="">
+                                    <div class="chat_block_chat_body_row_input">
+                                        <input type="text">
+                                        <span>Отправить</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="msg_block_getting_line_text">
-                            <span>${element.name}</span>
-                            <p>${element.msgBlock.msgs[element.msgBlock.msgs.length - 1].text}</p>
+                        <div class="chat_block_info">
+                            <div class="info_active_block_photo">
+                            
+                            </div>
+                            <span>Тех поддержка</span>
+                            <p>Администрация</p>
                         </div>
                     </div>
                 `);
-                
-                block.append(template_text);
-            })
 
-            $('.index_page_body_data').append(block);
+                $('.index_page_body_data').append(templateText);
+        };
+
+        async render()
+        {
+            var _idBlock = _GET("id");
+
+            if(_idBlock)
+            {
+                this.renderChat();
+            }
+            else
+            {
+                var getChats     = await callApi({
+                    methodName: "getChats",
+                    data:  global.allData._id,
+                });
+    
+                var block = $(`
+                    <div class="msg_block_getting">
+    
+                    </div>
+                `);
+    
+                getChats.forEach(element => {
+    
+                    var _PathUrl = null;
+    
+                    if(element.img)
+                    {
+                        _PathUrl = `https://api.telegram.org/file/bot2062839693:AAE0hzj8SVXyexq29s5x7aRLC5x8O77c-pQ/` + element.img.file_path;
+                    }
+    
+                    var template_text = $(`
+                        <div class="msg_block_getting_line">
+                            <div class="msg_block_getting_line_img">
+                                <div class="msg_block_getting_line_img_block">
+                                    <img src="${_PathUrl}" alt="">
+                                </div>
+                            </div>
+                            <div class="msg_block_getting_line_text">
+                                <span>${element.name}</span>
+                                <p>${element.msgBlock.msgs[element.msgBlock.msgs.length - 1].text}</p>
+                            </div>
+                        </div>
+                    `);
+                    
+                    block.append(template_text);
+                })
+    
+                $('.index_page_body_data').append(block);
+            }
         }
     }
 
