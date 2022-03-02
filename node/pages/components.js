26f67d648f22f82c9fd82ccb945047b2b54e5379
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-03-02 23:24:53 +0300

    aawd

--- node/pages/components.js
+++ node/pages/components.js
@@ -2088,7 +2088,9 @@ async function acceptInvestor(socket,data,callback)
             var HowManyDays     = Interval.fromDateTimes(NowToday, LastData).length('year');
             var EveryPayment    = Number(InvPay / 100 * RateBlock).toFixed(0);
 
-            for(var i = 0; i <= HowManyDays; i++)
+            console.log(HowManyDays);
+
+            for(var i = 0; i < HowManyDays; i++)
             {
                 InvPays.push({
                     pay: EveryPayment,
