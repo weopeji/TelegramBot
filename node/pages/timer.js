7bf5624f242cb0da0d78d36d73f1ee9a64102ef4
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-03-04 02:13:57 +0300

    awd

--- node/pages/timer.js
+++ node/pages/timer.js
@@ -13,11 +13,7 @@ function startTimer()
 {
     console.log("Start");
 
-    const d = new Date();
-
-    console.log(d);
-
-    const job = new CronJob('0 */1 2-3 * * *', function() {
+    const job = new CronJob('0 */1 23-1 * * *', function() {
         const d = new Date();
         console.log('Every 30 minutes between 9-17:', d);
     });
