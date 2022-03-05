262f58d2e50a49ef0b838dae58770a069afad769
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-03-05 04:01:24 +0300

    awd

--- html/assets/javascript/index/component.js
+++ html/assets/javascript/index/component.js
@@ -1171,11 +1171,11 @@
 
         async render() 
         {
-            $('.index_page_body_data').append(`
+            $('.index_page_body_data').append($(`
                 <div class="Attracted_headerInfoBlock_info_data_alert">
                     <span>Поступившие инвестиции необходимо подтвердить в течении 3х банковских дней</span>
                 </div>
-            `).css('font-size', '20px');
+            `).css('font-size', '20px'));
 
             var _id = global.allData._id;
 
