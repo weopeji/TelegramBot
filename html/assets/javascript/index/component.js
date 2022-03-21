f547f31e1a50f0a4516c04bbef2136eb1c0e81ec
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-03-21 13:57:50 +0300

    awd

--- html/assets/javascript/index/component.js
+++ html/assets/javascript/index/component.js
@@ -650,7 +650,6 @@
 
                                 headerPaysBlock.find('.Attracted_headerInfoBlock_info_data_alert_buttom_cheack').click( function() {
                                     // $('#Attracted_headerInfoBlock_info_data_alert_buttom_cheack_input').trigger('click');
-
                                     location.href = "https://invester-relocation.site/?page=activ_projects&action=investingNotFullNull";
                                 });
 
