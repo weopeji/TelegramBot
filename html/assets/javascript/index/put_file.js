745879be69b84546851fadbb4b84cb33ad4b4132
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-03-21 14:07:28 +0300

    Update put_file.js

--- html/assets/javascript/index/put_file.js
+++ html/assets/javascript/index/put_file.js
@@ -16,8 +16,24 @@
 
     class put_file
     {
+        defaultCSS()
+        {
+            $('.index_page_menu').css({
+                "flex": "none",
+                "position": "absolute",
+                "margin-left": "-100%",
+            });
+
+            $('.index_page_body_header_info').css({
+                "justify-content": "center",
+            });
+
+            $('.index_page_body_header_info span').html(`Инвестирование в проект "${this.project.data.name}"`);
+        }
+
         async render()
         {
+            this.defaultCSS();
             var getAction = _GET("action");
         };
     };
