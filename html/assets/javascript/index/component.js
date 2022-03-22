8ebc72256a1cd85b359a07c52ac233bf14be7a82
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-03-22 15:21:17 +0300

    awd

--- html/assets/javascript/index/component.js
+++ html/assets/javascript/index/component.js
@@ -765,6 +765,23 @@
                         {
                             if(typeof _data.InvDoc.data.pts_2 == "undefined")
                             {
+                                headerPaysBlock = $(`
+                                    <div class="Attracted_headerInfoBlock_info_data_alert">
+                                        <input type="file" id="Attracted_headerInfoBlock_info_data_alert_buttom_cheack_input">
+                                        <div class="Attracted_headerInfoBlock_info_data_alert_buttom_cheack">
+                                            <span>Прикрепить чек</span>
+                                        </div>
+                                    </div>
+                                `);
+
+                                headerPaysBlock.find('.Attracted_headerInfoBlock_info_data_alert_buttom_cheack').click( function() {
+                                    location.href = `https://invester-relocation.site/?page=put_file&action=investingNotFull&InvId=${_data.InvDoc._id}`;
+                                });
+
+                                $('.headerPaysBlock').remove();
+                            }
+                            else
+                            {
                                 if(typeof _data.InvDoc.confirmationData == "undefined")
                                 {
                                     headerPaysBlock = $(`
@@ -775,23 +792,6 @@
                                             </div>
                                         </div>
                                     `);
-
-                                    $('.headerPaysBlock').remove();
-                                }
-                                else
-                                {
-                                    headerPaysBlock = $(`
-                                        <div class="Attracted_headerInfoBlock_info_data_alert">
-                                            <input type="file" id="Attracted_headerInfoBlock_info_data_alert_buttom_cheack_input">
-                                            <div class="Attracted_headerInfoBlock_info_data_alert_buttom_cheack">
-                                                <span>Прикрепить чек</span>
-                                            </div>
-                                        </div>
-                                    `);
-
-                                    headerPaysBlock.find('.Attracted_headerInfoBlock_info_data_alert_buttom_cheack').click( function() {
-                                        location.href = `https://invester-relocation.site/?page=put_file&action=investingNotFull&InvId=${_data.InvDoc._id}`;
-                                    });
 
                                     $('.headerPaysBlock').remove();
                                 }
