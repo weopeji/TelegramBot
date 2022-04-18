923ed0086e145fcdf742715f1f5075acd0d86c98
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-04-18 15:09:54 +0300

    awd

--- html/assets/javascript/index/component.js
+++ html/assets/javascript/index/component.js
@@ -1691,8 +1691,11 @@
 
             var settingBlock = $(`
                 <div class="settingBlock">
-                    <div class="settingBlock_header">
+                    <div class="version2_settingBlock_header">
                         <p>Инвестора</p>
+                    </div>
+                    <div class="version2_default_bkg row_default"></div>
+                    <div class="settingBlock_header">
                         <div class="settingBlock_header_line">
                             <span>№</span> 
                             <span>Номер договора</span>
