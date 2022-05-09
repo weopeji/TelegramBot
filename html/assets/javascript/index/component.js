8152016aa913f7a850e681bf43a1614ef9e11f5a
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-05-09 12:34:19 +0300

    awd

--- html/assets/javascript/index/component.js
+++ html/assets/javascript/index/component.js
@@ -163,7 +163,7 @@
                         var cheackInitNumber = 0;
 
                         $('.settingBlock_body_line').each((i, elementLine) => {
-                            if($(elementLine).find(`input[type="checkbox"]`).attr("checked") == 'checked') {
+                            if($(elementLine).find(`input[type="checkbox"]`).is(":checked")) {
                                 cheackInitNumber++;
                             }
                         });
