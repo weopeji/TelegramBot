7af8a87334ae4664c649f122022fd580ec879169
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-05-14 15:56:00 +0300

    awd

--- node/pages/components.js
+++ node/pages/components.js
@@ -273,13 +273,13 @@ async function version2_wait_projects_WaitNotFullInvs(socket, data, callback)
     try {
         var _User           = await User.findOne({_id: data});
         var _InvsByWait     = await InvDoc.find({invester: _User.user, applicationRequest: true, status: "accept"});
-        var _AllInvsByUser  = await InvDoc.find({invester: _User.user, status: "accept"});
         var ActionData      = [];
 
         for(var element of _InvsByWait)
         {
-            var ActionInit  = 0;
-            var _Project    = await Project.findOne({_id: element.projectId});
+            var ActionInit      = 0;
+            var _Project        = await Project.findOne({_id: element.projectId});
+            var _AllInvsByUser  = await InvDoc.find({projectId: _Project._id, status: "accept"});
 
             _AllInvsByUser.forEach((element2, i2) => {
                 if(element2._id == element._id) {
