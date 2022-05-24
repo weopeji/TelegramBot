d255002971e2719feceaaceb42e34113559ca4c6
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-05-24 13:52:49 +0300

    1

---
+++ test_node.js
@@ -0,0 +1,35 @@
+const secret = "opeji";
+const repo = "/var/www/";
+
+const express           = require('express');
+const app               = express();
+var server              = require('http').createServer(app).listen(8080);
+let crypto              = require('crypto');
+const exec              = require('child_process').exec
+
+app.post('/webhook', function(req, res) {
+    req.on('data', function(chunk) 
+    {
+        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+        var data = JSON.parse(chunk);
+
+        if(data["commits"]["message"] == "1")
+        {
+            console.log('ok');
+        }
+        else
+        {
+
+        }
+
+        //if (req.headers['x-hub-signature'] == sig) {
+	  //  console.log('cd ' + repo + ' && git pull https://weopeji:135315Ab135315@github.com/weopeji/TelegramBot.git && pm2 restart app');
+        //    exec('cd ' + repo + ' && git pull https://weopeji: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@github.com/weopeji/TelegramBot.git && pm2 restart app');
+        //    exec('pm2 restart app');
+	  //
+        //    console.log('UPDATE');
+        //}
+    });
+
+    res.end();
+})
