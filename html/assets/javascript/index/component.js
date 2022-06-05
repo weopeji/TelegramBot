19a7c7758dcb604881ca3272228e9f97921bf1c0
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-06-05 17:08:21 +0300

    w

--- html/assets/javascript/index/component.js
+++ html/assets/javascript/index/component.js
@@ -997,6 +997,9 @@
         async renderType(allData)
         {
             try {
+                alert("okkk");
+
+                
                 var _data = await callApi({
                     methodName: "invester_status_project",
                     data: _GET('id'),
