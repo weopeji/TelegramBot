03c02187690783cb5c2945eb4c61df1e76a7211f
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-06-13 18:21:09 +0300

    ц

--- html/assets/javascript/index/applications.js
+++ html/assets/javascript/index/applications.js
@@ -35,7 +35,7 @@
             var settingBlock2 = $(`
                 <div class="settingBlock">
                     <div class="version2_settingBlock_header">
-                        <p></p>
+                        <p>Заявки</p>
                     </div>
                     <div class="version2_default_bkg row_default"></div>
                     <div class="settingBlock_header">
