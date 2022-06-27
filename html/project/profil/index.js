ca78917079983890500674e87f5465fcba52afbf
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-06-27 19:15:44 +0300

    w

--- html/project/profil/index.js
+++ html/project/profil/index.js
@@ -108,6 +108,18 @@
                                         };
                                     });
                                 }
+                                else if ($(this).attr('push') == "false")
+                                {
+                                    $(this).attr('push', true);
+
+                                    $('.arbitr_info_block_body .page_line').each( async function(i, element) {
+                                        if(i > 0)
+                                        {
+                                            $(element).fadeIn();
+                                            await sleep(500)
+                                        };
+                                    });
+                                }
                                 else
                                 {
                                     $(this).attr('push', true);
