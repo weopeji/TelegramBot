b7e1d6d0e04800bd7d606f65b2a543ca415cbdd7
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-07-13 15:50:08 +0300

    awd

--- node/models/user.js
+++ node/models/user.js
@@ -29,7 +29,7 @@ const UserShema = new mongoose.Schema({
     business_msgPut: String,
     socket: String,
     acceptGetDataOfUser: Boolean,
-    block: Boolean,
+    block: String,
 });
 
 mongoose.model('User', UserShema);
