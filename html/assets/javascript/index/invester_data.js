8f06aecc1b981b1befaadc7fe04eccb1563b2f9f
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-10-05 12:47:54 +0300

    123

--- html/assets/javascript/index/invester_data.js
+++ html/assets/javascript/index/invester_data.js
@@ -385,8 +385,8 @@
         {
             $('.creating_page').empty();
 
-            $('.version2_invester_data_backBlock_circule').off('click').click( function(e) {
-                $('.version2_invester_data_backBlock_circule').remove();
+            $('.version2_invester_data_backBlock_circule').click( function(e) {
+                e.preventDefault();
                 _this.cheackGetDocuments();
             });
 
