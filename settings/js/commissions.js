35e27d7fe090c1df6f643cc9e49943ed1f09050f
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2022-10-05 15:53:38 +0300

    123

--- settings/js/commissions.js
+++ settings/js/commissions.js
@@ -27,21 +27,24 @@
 
             var templateText = $(`
                 <div class="settingBlock">
-                    <div class="settingBlock_header">
+                    <div class="version2_settingBlock_header">
                         <p>Ожидают подтверждения</p>
+                    </div>
+                    <div class="version2_default_bkg row_default"></div>
+                    <div class="settingBlock_header">
                         <div class="settingBlock_header_line">
-                            <span>Номер проекта</span>
-                            <span>Название проекта</span>
-                            <span>Инвестор</span>
-                            <span>Сумма Инвестиции</span>
-                            <span>Комиссия InvestER</span>
-                            <span>Чек</span>
-                            <span>Действие</span>
-                            <span>Сообщение</span>
+                        <span>Номер проекта</span>
+                        <span>Название проекта</span>
+                        <span>Инвестор</span>
+                        <span>Сумма Инвестиции</span>
+                        <span>Комиссия InvestER</span>
+                        <span>Чек</span>
+                        <span>Действие</span>
+                        <span>Сообщение</span>
                         </div>
                     </div>
                     <div class="settingBlock_body">
-                       
+
                     </div>
                 </div>
             `);
@@ -93,21 +96,22 @@
 
             var templateText = $(`
                 <div class="settingBlock">
+                <div class="version2_settingBlock_header">
+                <p>Подтвержденные</p>
+                    </div>
+                    <div class="version2_default_bkg row_default"></div>
                     <div class="settingBlock_header">
-                        <p>Подтвержденные</p>
-                        <div class="settingBlock_header_line">
-                            <span>Номер проекта</span>
-                            <span>Название проекта</span>
-                            <span>Инвестор</span>
-                            <span>Сумма Инвестиции</span>
-                            <span>Комиссия InvestER</span>
-                            <span>Чек</span>
-                            <span>Действие</span>
-                            <span>Сообщение</span>
-                        </div>
+                    <span>Номер проекта</span>
+                    <span>Название проекта</span>
+                    <span>Инвестор</span>
+                    <span>Сумма Инвестиции</span>
+                    <span>Комиссия InvestER</span>
+                    <span>Чек</span>
+                    <span>Действие</span>
+                    <span>Сообщение</span>
                     </div>
                     <div class="settingBlock_body">
-                       
+
                     </div>
                 </div>
             `);
